03aad3bf055d900a4e70e7555ae01b003fba7135
zhichang chen <chenzhichang@codemao.cn>
2021-11-29 17:11:53 +0800

    1. update gradle version 2. support androidx

--- AtraceHookSample/src/main/java/com/dodola/atrace/MainActivity.java
+++ AtraceHookSample/src/main/java/com/dodola/atrace/MainActivity.java
@@ -2,7 +2,6 @@ package com.dodola.atrace;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -10,14 +9,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean b = Atrace.hasHacks();
-                if (b) {
-                    Atrace.enableSystrace();
-                    Toast.makeText(MainActivity.this, "开启成功", Toast.LENGTH_SHORT).show();
-                }
+        findViewById(R.id.button).setOnClickListener(v -> {
+            boolean b = Atrace.hasHacks();
+            if (b) {
+                Atrace.enableSystrace();
+                Toast.makeText(MainActivity.this, "开启成功", Toast.LENGTH_SHORT).show();
             }
         });
     }
